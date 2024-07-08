2ee5d67f3ac1626ec7e729a5380f5aae5b106f26
jeremy duhem <redkardss@gmail.com>
2024-07-08 10:33:42 +0200

    création exoAngular

--- src/app/components/testformulaire/testformulaire.component.ts
+++ src/app/components/testformulaire/testformulaire.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-testformulaire',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './testformulaire.component.html',
-  styleUrl: './testformulaire.component.css'
+  styleUrl: './testformulaire.component.css',
 })
 export class TestformulaireComponent {
+  article: FormGroup;
 
+  listeArticles: any[] = [];
+
+  constructor() {
+    this.article = new FormGroup({
+      nom: new FormControl('', Validators.required),
+      prix: new FormControl('', Validators.required),
+      designation: new FormControl('', Validators.required),
+    });
+  }
+
+  addArticle() {
+    this.listeArticles.push(this.article.value);
+
+    this.article.reset();
+  }
+
+  get totalPrice(): number {
+    return this.listeArticles.reduce(
+      (total, article) => total + article.prix,
+      0
+    );
+  }
 }
